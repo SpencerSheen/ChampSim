938bf165d101dcbc879a728f7ca34b137fb2ceb1
Spencer Sheen <spsheen97@gmail.com>
2022-01-27 20:04:26 -0800

    final commit

---
+++ run_default.sh
@@ -0,0 +1,6 @@
+PREF=${1}
+
+./run_champsim.sh bimodal-${PREF}-no-no-no-lru-1core 50 50 server_036.champsimtrace.xz
+./run_champsim.sh bimodal-${PREF}-no-no-no-lru-1core 50 50 server_037.champsimtrace.xz
+./run_champsim.sh bimodal-${PREF}-no-no-no-lru-1core 50 50 server_038.champsimtrace.xz
+./run_champsim.sh bimodal-${PREF}-no-no-no-lru-1core 50 50 server_039.champsimtrace.xz
